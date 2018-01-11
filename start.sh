39e8519c066a0ac6b41c0b6fd2766084c41e4a6a
Ben Fairfield <benfa@umich.edu>
2018-01-11 14:33:01 -0500

    now using it-dev not its-dev site content

---
+++ start.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Redirect logs to stdout and stderr for docker reasons.
+ln -sf /dev/stdout /var/log/apache2/access_log
+ln -sf /dev/stderr /var/log/apache2/error_log
+
+ln -sf /secrets/apache2/apache2.conf /etc/apache2/sites-available/apache2.conf
+ln -sf /secrets/apache2/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
+
+ln -sf /secrets/app/settings.php /var/www/html/sites/default/settings.php
+
+ln -sf /secrets/ssl/USERTrustRSACertificationAuthority.pem /etc/ssl/certs/USERTrustRSACertificationAuthority.pem
+ln -sf /secrets/ssl/AddTrustExternalCARoot.pem /etc/ssl/certs/AddTrustExternalCARoot.pem
+ln -sf /secrets/ssl/sha384-Intermediate-cert.pem /etc/ssl/certs/sha384-Intermediate-cert.pem
+ln -sf /secrets/ssl/it-dev.openshift.dsc.umich.edu.cert /etc/ssl/certs/it-dev.openshift.dsc.umich.edu.cert
+ln -sf /secrets/ssl/it-dev.openshift.dsc.umich.edu.key /etc/ssl/private/it-dev.openshift.dsc.umich.edu.key
+
+## Rehash command needs to be run before starting apache.
+c_rehash /etc/ssl/certs
+
+a2enmod ssl
+a2enmod include
+a2ensite default-ssl 
+
+## set SGID for www-data 
+chown -R www-data.www-data /var/www/html /var/cosign
+chmod -R 2775 /var/www/html /var/cosign
+
+/usr/local/bin/apache2-foreground
