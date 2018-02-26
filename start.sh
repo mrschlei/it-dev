c46581f7caf62d5e282771fef27b6827aed87cfb
Ben Fairfield <benfa@umich.edu>
2018-02-26 10:52:52 -0500

    start.sh now calls local.start.sh

--- start.sh
+++ start.sh
@@ -4,23 +4,23 @@
 ln -sf /dev/stdout /var/log/apache2/access_log
 ln -sf /dev/stderr /var/log/apache2/error_log
 
-# apache and virtual host secrets
+# apache secrets
 ln -sf /secrets/apache2/apache2.conf /etc/apache2/apache2.conf
 ln -sf /secrets/apache2/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
 ln -sf /secrets/apache2/cosign.conf /etc/apache2/mods-available/cosign.conf
-
-# app secrets
-#ln -sf /secrets/app/settings.php /var/www/html/sites/default/settings.php
 
 # SSL secrets
 ln -sf /secrets/ssl/USERTrustRSACertificationAuthority.pem /etc/ssl/certs/USERTrustRSACertificationAuthority.pem
 ln -sf /secrets/ssl/AddTrustExternalCARoot.pem /etc/ssl/certs/AddTrustExternalCARoot.pem
 ln -sf /secrets/ssl/sha384-Intermediate-cert.pem /etc/ssl/certs/sha384-Intermediate-cert.pem
-#ln -sf /secrets/ssl/it-dev.openshift.dsc.umich.edu.cert /etc/ssl/certs/it-dev.openshift.dsc.umich.edu.cert
-#ln -sf /secrets/ssl/it-dev.openshift.dsc.umich.edu.key /etc/ssl/private/it-dev.openshift.dsc.umich.edu.key
 
 ## Rehash command needs to be run before starting apache.
 c_rehash /etc/ssl/certs
+
+if [ -f /secrets/app/local.start.sh ]
+then
+  /bin/sh /secrets/app/local.start.sh
+fi
 
 a2enmod ssl
 a2enmod include
